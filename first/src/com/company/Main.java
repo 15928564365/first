6124426dfbcc101a194b441e69d7cf617b922c28
bidaiqiang <737826489@qq.com>
2021-04-06 14:41:54 +0800

    创建检测实体

--- first/src/com/company/Main.java
+++ first/src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello ");
+        TestModel testModel = new TestModel();
+        testModel.setId(1);
+        System.out.println(testModel.toString());
     }
 }
