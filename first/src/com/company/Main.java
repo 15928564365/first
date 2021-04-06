1876567da54f33a4f7c24d7b231316e782159ed5
bidaiqiang <737826489@qq.com>
2021-04-06 14:53:34 +0800

    Revert "创建检测实体"
    This reverts commit 6124426d

--- first/src/com/company/Main.java
+++ first/src/com/company/Main.java
@@ -4,8 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello ");
-        TestModel testModel = new TestModel();
-        testModel.setId(1);
-        System.out.println(testModel.toString());
     }
 }
