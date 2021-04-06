f94bc82ca644ca005e4033a95524fdc60832befd
bidaiqiang <737826489@qq.com>
2021-04-06 14:31:17 +0800

    创建检测实体

---
+++ first/src/com/company/TestModel.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * @Description:
+ * @Author: bdq
+ * @Date: 14:30 2021/4/6
+ */
+public class TestModel {
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
